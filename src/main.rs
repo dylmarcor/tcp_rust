5ffa8a46d22994c7ec1f7643a85b9b16ad607844
Dylmarcor <dylancorbett@icloud.com>
2019-07-29 15:08:34 -0700

    Fix tun-tap dependency

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("failed to cr")?;
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut bug = [0u8; 1504];
     let read = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x}", nbytes, &buf[..nbytes]);
+    eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
     Ok(())
 }
